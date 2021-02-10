d2a6167ba92a6183c4f42823dc1d74f04e676336
sait0928 <y.saitou928@gmail.com>
2021-02-10 18:29:45 +0900

    ミスの修正とコメントの追加

--- shortest_path.php
+++ shortest_path.php
@@ -18,6 +18,7 @@ for ($i = 0; $i < $n_point; $i ++) {
 	$s_route[$i] = -1; // 最短経路上の地点番号
 	$p_dist[$i] = 10000; // 出発地から各地点までの最短距離
 	$p_fixed[$i] = false; // 各地点の最短距離の確定状態
+	$p_route[$i] = 0; // 出発地から最短経路で各地点にたどり着く時、直前に経由する地点番号
 }
 
 $sp = 0; // 出発地の地点番号
@@ -26,15 +27,15 @@ $p_dist[$sp] = 0; // 出発地から出発地自身への最短距離は0
 
 // 最短経路探索処理
 while (true) {
-	// 最短距離が未確定の地点を1つ探す
+	// 最短距離が未確定の地点(p_fixed[i]がfalseの地点)を1つ探す
 	for ($i = 0; $i < $n_point; $i ++) {
 		if (!$p_fixed[$i]) {
 			break;
 		}
 	}
 
-	// 出発地から全地点までの最短距離が確定したら、探索処理終了
-	if ($i = $n_point) {
+	// 出発地から全地点までの最短距離が確定したら、探索処理終了(直前のループでbreakできなかった場合)
+	if ($i == $n_point) {
 		break;
 	}
 
@@ -50,12 +51,14 @@ while (true) {
 	$p_fixed[$s_point] = true; // 出発地からの最短距離を確定
 
 	for ($j = 0; $j < $n_point; $j ++) {
+		// jまでの最短経路が確定されていない時、s_pointを経由した場合のjまでの距離を求める
 		if ($distance[$s_point][$j] > 0 && !($p_fixed[$j])) {
 			$new_dist = $p_dist[$s_point] + $distance[$s_point][$j];
 
+			// これが現時点のp_dist[j]の値よりも小さかった場合は更新する
 			if ($new_dist < $p_dist[$j]) {
-				$p_dist[$j] = $new_dist;
-				$p_route[$j] = $s_point;
+				$p_dist[$j] = $new_dist; // 最短距離の上書き
+				$p_route[$j] = $s_point; // 直前の地点番号
 			}
 		}
 	}
@@ -63,13 +66,13 @@ while (true) {
 
 $dp = 6; // 目的地の地点番号
 
-$s_dist = $p_dist[$dp];
+$s_dist = $p_dist[$dp]; // 目的地までの最短距離
 
 $j = 0;
 $i = $dp;
 while ($i != $sp) {
-	$s_route[$j] = $i;
-	$i = $p_route[$i];
+	$s_route[$j] = $i; // 最短経路の地点番号を目的地から順番に格納
+	$i = $p_route[$i]; // 直前の地点番号を辿っていく
 	$j ++;
 }
 
